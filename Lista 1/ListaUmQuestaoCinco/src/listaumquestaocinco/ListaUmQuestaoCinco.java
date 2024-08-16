ca59cb8fd4efebca5d6e0a60dd7358558e0e1e72
Leandro de Almeida Oliveira <90487905+leomitx10@users.noreply.github.com>
2024-08-16 08:32:47 -0300

    Update ListaUmQuestaoCinco.java

--- Lista 1/ListaUmQuestaoCinco/src/listaumquestaocinco/ListaUmQuestaoCinco.java
+++ Lista 1/ListaUmQuestaoCinco/src/listaumquestaocinco/ListaUmQuestaoCinco.java
@@ -2,39 +2,23 @@ package listaumquestaocinco;
 
 import java.util.Scanner;
 
-/** ** @author Leandro*/
+/** @author Leandro */
 public class ListaUmQuestaoCinco {
     public static void main(String[] args) {
-    
-    Scanner scan = new Scanner(System.in);
-    int dia = scan.nextInt();
-    
-    switch(dia){
-        case 1:
-                System.out.println("Segunda-feira");
-                break;
-            case 2:
-                System.out.println("Terca-feira");
-                break;
-            case 3:
-                System.out.println("Quarta-feira");
-                break;
-            case 4:
-                System.out.println("Quinta-feira");
-                break;
-            case 5:
-                System.out.println("Sexta-feira");
-                break;
-            case 6:
-                System.out.println("Sabado");
-                break;
-            case 7:
-                System.out.println("Domingo");
-                break;
-            default:
-                System.out.println("O numero deve ser de 1 a 7");
-                break;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um numero de 1 a 7: ");
+        int dia = scan.nextInt();
+
+        String[] diaSemana = { "Segunda-feira", "Terca-feira", "Quarta-feira", 
+                                "Quinta-feira", "Sexta-feira", "Sabado", "Domingo"};
+
+        if (dia >= 1 && dia <= 7) {
+            System.out.println(diaSemana[dia - 1]);
+        } else {
+            System.out.println("Numero invalido. Digite um numero de 1 a 7.");
         }
-    scan.close();
+
+        scan.close();
     }
 }
