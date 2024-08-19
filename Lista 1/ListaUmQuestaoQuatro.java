cbf552d3453d48bef1ae0a47f535d058f4f4c545
Leandro de Almeida Oliveira <90487905+leomitx10@users.noreply.github.com>
2024-08-19 07:27:51 -0300

    Update ListaUmQuestaoQuatro.java

--- Lista 1/ListaUmQuestaoQuatro.java
+++ Lista 1/ListaUmQuestaoQuatro.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 /** ** @author Leandro*/
 public class ListaUmQuestaoQuatro {
     public static void main(String[] args) {
+        System.out.println("Milhas: ");
         Scanner scan = new Scanner(System.in);
         
         float m = scan.nextFloat();
         
         float km = m * 1.609f;
         
-        System.out.println("km: " + km);
+        System.out.println("Km: " + km);
         
         scan.close(); 
     }
